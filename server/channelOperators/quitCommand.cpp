f44a5bbe79933950f5cac0dbac90fc352eb14aa7
Neddy <joaoped2joaoped2@gmail.com>
2024-07-11 16:17:04 +0100

    Fixed seg fault

--- server/channelOperators/quitCommand.cpp
+++ server/channelOperators/quitCommand.cpp
@@ -5,8 +5,23 @@
 void Server::quitCommand(std::vector<std::string> tokens, Client *user) {
 	(void) tokens;
 	sendMessage(user->getSocketFD(), ":" + user->getNick() + " ERROR " + ":Leaving\r\n");
+    for (std::map<std::string, Channel*>::iterator it = _channelsList.begin(); it != _channelsList.end();) {
+        Channel* channel = it->second;
+        channel->removeUser(user);
+        if (channel->getNumOfMembers() == 1 &&  channel->getMembersForList() == "BOT") {
+            channel->removeBotFromChannel();
+            delete it->second;
+
+            std::map<std::string, Channel*>::iterator eraseIt = it;
+            ++it;
+            _channelsList.erase(eraseIt);
+        }
+        else
+            ++it;
+    }
 	std::map<int, Client *>::iterator it = _tmpClients.find(user->getSocketFD());
 	if (it != _tmpClients.end())
         delete it->second;
 		_tmpClients.erase(it);
+
 }
