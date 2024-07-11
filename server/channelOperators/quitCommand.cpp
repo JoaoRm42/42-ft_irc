3b9f934fa61751274f967876fd21f9ad39ae0e6d
macastan <mluizacmarques@gmail.com>
2024-07-11 16:05:59 +0100

    remove the user on the channel they participate

--- server/channelOperators/quitCommand.cpp
+++ server/channelOperators/quitCommand.cpp
@@ -4,9 +4,24 @@
 
 void Server::quitCommand(std::vector<std::string> tokens, Client *user) {
 	(void) tokens;
+	removeUsersChannels(user);
 	sendMessage(user->getSocketFD(), ":" + user->getNick() + " ERROR " + ":Leaving\r\n");
 	std::map<int, Client *>::iterator it = _tmpClients.find(user->getSocketFD());
 	if (it != _tmpClients.end())
         delete it->second;
 		_tmpClients.erase(it);
+}
+
+void	Server::removeUsersChannels(Client *user) {
+	for (size_t k = 0; k < user->getChannels().size(); k++)
+	{
+		for (std::map<std::string, Channel *>::iterator it = _channelsList.begin(); it != _channelsList.end(); ++it) {
+			if (it->first == user->getChannels()[k])
+			{
+				it->second->removeUser(user);
+				if (it->second->getNumOfMembers() == 0)
+					removeChannel(it->first);
+			}
+		}
+	}
 }
