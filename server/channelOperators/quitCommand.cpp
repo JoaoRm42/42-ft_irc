681fb2631d6409dcea9c785365e37b0961884d4f
Neddy <joaoped2joaoped2@gmail.com>
2024-07-11 17:39:08 +0100

    Commented what it was giving me segfault, and replaced it with the old code

--- server/channelOperators/quitCommand.cpp
+++ server/channelOperators/quitCommand.cpp
@@ -3,6 +3,30 @@
 #include "../../libs.hpp"
 
 void Server::quitCommand(std::vector<std::string> tokens, Client *user) {
+    (void) tokens;
+    for (std::map<std::string, Channel*>::iterator it = _channelsList.begin(); it != _channelsList.end();) {
+        Channel* channel = it->second;
+        channel->removeUser(user);
+        if (channel->getNumOfMembers() == 1 &&  channel->getMembersForList() == "BOT") {
+            channel->removeBotFromChannel();
+            delete it->second;
+
+            std::map<std::string, Channel*>::iterator eraseIt = it;
+            ++it;
+            _channelsList.erase(eraseIt);
+        }
+        else
+            ++it;
+    }
+    std::map<int, Client *>::iterator it = _tmpClients.find(user->getSocketFD());
+    if (it != _tmpClients.end())
+        delete it->second;
+    _tmpClients.erase(it);
+
+}
+
+/*
+void Server::quitCommand(std::vector<std::string> tokens, Client *user) {
 	(void) tokens;
 	close(user->getSocketFD());
 	removeUsersChannels(user);
@@ -29,3 +53,4 @@ void	Server::removeUsersChannels(Client *user) {
 		}
 	}
 }
+ */
