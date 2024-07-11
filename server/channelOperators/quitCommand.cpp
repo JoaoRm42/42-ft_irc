55954f37b6c063f9205c0437eb388a325af055c2
Neddy <joaoped2joaoped2@gmail.com>
2024-07-11 16:19:31 +0100

    Working for now in Windows

--- server/channelOperators/quitCommand.cpp
+++ server/channelOperators/quitCommand.cpp
@@ -4,7 +4,7 @@
 
 void Server::quitCommand(std::vector<std::string> tokens, Client *user) {
 	(void) tokens;
-	removeUsersChannels(user);
+//	removeUsersChannels(user);
 	sendMessage(user->getSocketFD(), ":" + user->getNick() + " ERROR " + ":Leaving\r\n");
     for (std::map<std::string, Channel*>::iterator it = _channelsList.begin(); it != _channelsList.end();) {
         Channel* channel = it->second;
@@ -27,16 +27,16 @@ void Server::quitCommand(std::vector<std::string> tokens, Client *user) {
 
 }
 
-void	Server::removeUsersChannels(Client *user) {
-	for (size_t k = 0; k < user->getChannels().size(); k++)
-	{
-		for (std::map<std::string, Channel *>::iterator it = _channelsList.begin(); it != _channelsList.end(); ++it) {
-			if (it->first == user->getChannels()[k])
-			{
-				it->second->removeUser(user);
-				if (it->second->getNumOfMembers() == 0)
-					removeChannel(it->first);
-			}
-		}
-	}
-}
+//void	Server::removeUsersChannels(Client *user) {
+//	for (size_t k = 0; k < user->getChannels().size(); k++)
+//	{
+//		for (std::map<std::string, Channel *>::iterator it = _channelsList.begin(); it != _channelsList.end(); ++it) {
+//			if (it->first == user->getChannels()[k])
+//			{
+//				it->second->removeUser(user);
+//				if (it->second->getNumOfMembers() == 0)
+//					removeChannel(it->first);
+//			}
+//		}
+//	}
+//}
