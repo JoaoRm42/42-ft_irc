74125fa433bb55287687add5d9b84a33fee7a01f
ccosta-c <ccosta-c@student.42porto.com>
2024-07-11 16:39:56 +0100

    fixed BROKENPIPE

--- Makefile
+++ Makefile
@@ -11,11 +11,11 @@
 # **************************************************************************** #
 
 NAME = ircserv
-CXX = c++
-CXXFLAGS = -std=c++98 -Wall -Wextra -Werror -g
+#CXX = c++
+#CXXFLAGS = -std=c++98 -Wall -Wextra -Werror -g
 #-------------------FOR CLION-------------------
-#CXX = g++
-#CXXFLAGS = -std=c++98 -g
+CXX = g++
+CXXFLAGS = -std=c++98 -g
 
 SRCS = main.cpp \
 	server/server.cpp \
--- server/channelOperators/quitCommand.cpp
+++ server/channelOperators/quitCommand.cpp
@@ -4,22 +4,9 @@
 
 void Server::quitCommand(std::vector<std::string> tokens, Client *user) {
 	(void) tokens;
-//	removeUsersChannels(user);
+	close(user->getSocketFD());
+	removeUsersChannels(user);
 	sendMessage(user->getSocketFD(), ":" + user->getNick() + " ERROR " + ":Leaving\r\n");
-    for (std::map<std::string, Channel*>::iterator it = _channelsList.begin(); it != _channelsList.end();) {
-        Channel* channel = it->second;
-        channel->removeUser(user);
-        if (channel->getNumOfMembers() == 1 &&  channel->getMembersForList() == "BOT") {
-            channel->removeBotFromChannel();
-            delete it->second;
-
-            std::map<std::string, Channel*>::iterator eraseIt = it;
-            ++it;
-            _channelsList.erase(eraseIt);
-        }
-        else
-            ++it;
-    }
 	std::map<int, Client *>::iterator it = _tmpClients.find(user->getSocketFD());
 	if (it != _tmpClients.end())
         delete it->second;
@@ -27,16 +14,16 @@ void Server::quitCommand(std::vector<std::string> tokens, Client *user) {
 
 }
 
-//void	Server::removeUsersChannels(Client *user) {
-//	for (size_t k = 0; k < user->getChannels().size(); k++)
-//	{
-//		for (std::map<std::string, Channel *>::iterator it = _channelsList.begin(); it != _channelsList.end(); ++it) {
-//			if (it->first == user->getChannels()[k])
-//			{
-//				it->second->removeUser(user);
-//				if (it->second->getNumOfMembers() == 0)
-//					removeChannel(it->first);
-//			}
-//		}
-//	}
-//}
+void	Server::removeUsersChannels(Client *user) {
+	for (size_t k = 0; k < user->getChannels().size(); k++)
+	{
+		for (std::map<std::string, Channel *>::iterator it = _channelsList.begin(); it != _channelsList.end(); ++it) {
+			if (it->first == user->getChannels()[k])
+			{
+				it->second->removeUser(user);
+				if (it->second->getNumOfMembers() == 0)
+					removeChannel(it->first);
+			}
+		}
+	}
+}
